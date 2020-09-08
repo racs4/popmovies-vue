78214e87c5d795b09108bc97a463899f81bd181c
Rheidner <rheidner.silva@napista.com.br>
2020-09-08 07:33:23 -0300

    feat: cria component secao

---
+++ src/components/Section.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="mt-5 mb-5">
+      <h1 class="mb-5" :class="{'text-right': titleRight}">
+          {{title}}
+      </h1>
+      <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    titleRight: {
+      type: Boolean,
+      default() { return false; },
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
